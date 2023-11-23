95a8f01bc8de616c7c1c2e12ed861fd6a3df15b2
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2023-11-23 16:26:44 +0100

    Initial commit

---
+++ SQL.py
@@ -0,0 +1,15 @@
+
+artikel = """SELECT * FROM [emea_enventa_live].[dbo].[ARTIKEL]"""
+
+artikeleinheit = "SELECT *  FROM [emea_enventa_live].[dbo].[ARTIKELEINHEIT]"
+
+artikeltext = """SELECT * FROM [emea_enventa_live].[dbo].[ARTIKELTEXT]"""
+
+produktentwicklung = "SELECT *  FROM [emea_enventa_live].[dbo].[P14_ArticleCosmetics]"
+
+rabatt_kopf = "SELECT *  FROM [emea_enventa_live].[dbo].[EINKRABATT]"
+
+rabatt_gueltig = "SELECT *  FROM [emea_enventa_live].[dbo].[BRABATTGUELTIGKEIT]"
+
+rabatt_staffel = "SELECT *  FROM [emea_enventa_live].[dbo].[EINKRABATTSTAFFEL]"
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import pyodbc
+import pandas as pd
+import SQL as s
+from datetime import datetime
+import time
+from tkinter import messagebox
+
+
+
+
+#start = time.time()
+
+# Query Connection
+connx_string = r"DRIVER={SQL Server}; server=172.19.128.2\emeadb; database=emea_enventa_live; UID=usr_razek; PWD=wB382^%H3INJ"
+conx = pyodbc.connect(connx_string)
+
+datum = str(datetime.now())[0:10]
+
+sqls = [[s.artikel, 'Artikelstamm'], [s.artikeleinheit, 'Artikeleinheit'], [s.artikeltext, 'Artikelbeschreibung'], [s.produktentwicklung, 'Produktentwicklung'],
+        [s.rabatt_kopf, 'Rabatt_Kopf'], [s.rabatt_gueltig, 'Rabatt_Gueltig'], [s.rabatt_staffel, 'Rabatt_Staffel']]
+
+
+for sql in sqls:
+    try:
+        df= pd.read_sql(sql[0], conx)
+        df.to_excel(fr'S:\EMEA\Aenderungsprotokoll\{sql[1]}\{sql[1]}_{datum}.xlsx', index=False)
+    except PermissionError:
+        messagebox.showinfo('Excel offen!', f'Bitte schließe die Excel Liste: {sql[1]} und wiederhole den Vorgang.')
+        break
+    except OSError:
+        messagebox.showinfo('Ordner Gelöscht', f'Kontrolliere ob der Ordner: {sql[1]} vorhanden ist und wiederhole den Vorgang.')
+        break
+
+#end = time.time()
+print('Backup abgeschlossen')
+
+messagebox.showinfo('Enventa Backup Erfolgreich!', 'Es wurde erfolgreich ein Backup angelegt.')
+
